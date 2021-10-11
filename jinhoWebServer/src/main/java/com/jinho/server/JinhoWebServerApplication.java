d75bef3c8ebce768cb15b10754b717d579d99816
ace2267 <ace22672@gmail.com>
2021-10-12 08:53:04 +0900

    was sample init

---
+++ jinhoWebServer/src/main/java/com/jinho/server/JinhoWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.jinho.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinhoWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinhoWebServerApplication.class, args);
+	}
+
+}
---
+++ jinhoWebServer/src/test/java/com/jinho/server/JinhoWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinho.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinhoWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
